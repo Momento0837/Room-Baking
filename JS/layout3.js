19daeef9c8d1d9f8a3c4719c4cd2099379d470c0
Momento0837 <momento0837@gmail.com>
2025-05-31 16:37:01 +0900

    메인페이지 전환화면 추가

--- JS/layout3.js
+++ JS/layout3.js
@@ -13,9 +13,11 @@ $(document).ready(function(){
         $(this).stop().animate({ width: '30.5%', height: '100.5%' }, mid, 'easeOutQuad');
     }).on("mouseout", function() {
         $(this).stop().animate({ width: '30%', height: '100%' }, mid, 'easeOutQuad');
-        purseAnimation(); // html 펄스 애니메이션 재시작
     }).on("click", function(){
-        window.open('layout4.html', '_blank'); // 클릭시 html 메인페이지로 이동
+        $(".whitebox").css({display: 'block'});
+        $(".whitebox").animate({opacity: '100%'}, long, 'easeOutQuad', function(){
+            window.open('layout4.html', '_blank'); // 클릭시 html 메인페이지로 이동
+        });
     });
 
     // css 물감통에 마우스를 올리면 조금 커지고, 마우스를 떼면 다시 본래 크기로 돌아감
@@ -23,7 +25,6 @@ $(document).ready(function(){
         $(this).stop().animate({ width: '20.5%', height: '25.6%', left: '29.75%', top: '45.5%' }, mid, 'easeOutQuad');
     }).on("mouseout", function() {
         $(this).stop().animate({ width: '20%', height: '25%', left: '30%', top: '46%' }, mid, 'easeOutQuad');
-        purseAnimation();; // css 펄스 애니메이션 재시작
     }).on("click", function(){
         window.open('layout6.html', '_blank'); // 클릭시 css 메인페이지로 이동
     });
